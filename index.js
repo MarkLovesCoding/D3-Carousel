0d9d873b1a657785b33936ee9c440224f1c75b62
Mark Halstead <mark.d.halstead@gmail.com>
2020-01-19 13:03:02 -0700

    Carousel-working
    Impliment Glide.js on generic carousel

---
+++ index.js
@@ -0,0 +1,35 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+
+
+ const express = require('express');
+ const path = require('path');
+ const router = express.Router();
+
+/**
+ * App Variables
+ */
+ const app = express();
+ const port = process.env.PORT || "8000";
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static('public'))
+/**
+ * Routes Definitions
+ */
+ router.get("/", (req, res) => {
+   res.sendFile(path.join(__dirname+'/views/index.html'));
+
+ });
+ app.use('/',router)
+/**
+ * Server Activation
+ */
+ app.listen(port, () => {
+   console.log(`Listening to requests on http://localhost:${port}`);
+ });
