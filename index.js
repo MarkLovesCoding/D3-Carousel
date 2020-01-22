a2a97793da4d66e4884d0da7baba1f792ad0d9d1
Mark Halstead <mark.d.halstead@gmail.com>
2020-01-21 21:28:11 -0700

    CSS to SCSS
    Modularize,
    Update packages to include node-sass
    Update package.json to include build-css script

--- index.js
+++ index.js
@@ -8,7 +8,8 @@
  const express = require('express');
  const path = require('path');
  const router = express.Router();
-
+const sass = require('node-sass');
+const fs = require('fs')
 /**
  * App Variables
  */
@@ -30,6 +31,30 @@ app.use(express.static('public'))
 
  });
  app.use('/',router)
+
+ sass.render({
+     // file or data must be specified. In this case we're telling node-sass there's a SCSS file at source/style.scss
+     file:__dirname+'/public/scss/style.scss'
+   },
+   // node-sass supports standard nod e style asynchronous callbacks with the signature of function(err, result).
+   // In error conditions, the error argument is populated with the error object.
+   // In success conditions, the result object is populated with an object describing the result of the render call.
+   (error, result) => {
+     if (!error) {
+       // Asynchronously writes data to a file, replacing the file if it already exists.
+       // In this case the file it's writing to is at `public/style.css`
+       // The `result` object contains `css`, the compiled CSS. This is what's written to the style.css file
+       fs.writeFile(__dirname+'/public/css/style.css', result.css.toString(), error => {
+         if (error) {
+           console.log(error,"sass write error");
+         }
+       });
+     }
+     else{
+       console.log(error,"sass render error")
+     }
+   }
+ );
 /**
  * Server Activation
  */
